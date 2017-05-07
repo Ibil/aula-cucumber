07fff0df4a70bceb0c471ecc740385e8d103ce44
Rui Maranhao <rui@computer.org>
2017-05-07 23:05:55 +0100

    clean imports

--- bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/cucumber/runner/RunBankConstructorTest.java
+++ bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/cucumber/runner/RunBankConstructorTest.java
@@ -1,17 +1,7 @@
 package pt.ulisboa.tecnico.softeng.bank.domain.cucumber.runner;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import cucumber.api.junit.Cucumber;
-import pt.ulisboa.tecnico.softeng.bank.domain.Account;
-import pt.ulisboa.tecnico.softeng.bank.domain.Bank;
-import pt.ulisboa.tecnico.softeng.bank.domain.Client;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import org.junit.runner.RunWith;
 
